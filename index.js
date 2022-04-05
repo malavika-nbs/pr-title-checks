a7566c4fc0b517a28c35f9e07d02ae4713f332b8
malavika-nbs <malavika.gotham@nationwide.co.uk>
2022-04-05 18:33:30 +0100

    Testing github PR title validations

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ async function run() {
         }
 
         const description = pullRequest.description;
+        core.info(`Pull Request description: "${description}"`);
         if(!description){
             core.setFailed(`The description of the Pull Request is empty`);
             return
